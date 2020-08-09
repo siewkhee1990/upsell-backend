77867f985eb9d863085a76e46dcb03b3ea1dc176
Shi Jie Teo <teoshijie@gmail.com>
2020-08-09 15:02:32 +0800

    herokubackend

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-//Dependencies 
+//Dependencies
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const frontEndUrl = process.env.Front_End_URL || 'http://localhost:3000';
 
 require('./db');
 
+//middlewares
 
-//middlewares 
-app.use(cors({origin: 'http://localhost:3000' , credentials: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", frontEndUrl);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Cookie, Content-Type, Accesstoken, ApiAccessToken, Accept, AppID");
+    next();
+});
+
 app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
 app.use(express.json());
 app.use(cookieParser());
@@ -24,7 +31,7 @@ require('./routes')(app);
 
 
 
-//this will catch any route that doesn't exist 
+//this will catch any route that doesn't exist
 app.get('*', (req, res) => {
     res.status(404).json('Sorry, page not found')
 })
