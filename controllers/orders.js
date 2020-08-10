684af5b67696498bb177ca36de1b7522afe3b5fd
Danny <siewkhee1990@hotmail.com>
2020-08-11 00:40:03 +0800

    added get all orders by id

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const Listings = require('../models/listings.js');
 const Orders = require('../models/orders')
 
-router.get('/', (req, res) => {
-    Orders.find({}, (err, foundOrders) => {
-        res.json(foundOrders);
+// find by userID route
+router.get('/:userID', (req, res) => {
+    Orders.find({ purchaser_userID: req.params.userID }, (err, foundOrders) => {
+        if (err) {
+            res.status(500).json({ message: { msgbody: err, msgError: true } })
+        } else {
+            res.json(foundOrders);
+        }
     });
 });
 
